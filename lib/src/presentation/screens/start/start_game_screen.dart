838e9defb942a3fca150b1536bb1c230703069f2
Fady Zaher <fady33zaher@gmail.com>
2023-11-30 16:07:23 +0200

    Tic Tac Game Start Screen

--- lib/src/presentation/screens/start/start_game_screen.dart
+++ lib/src/presentation/screens/start/start_game_screen.dart
@@ -10,6 +10,7 @@ class StartGameScreen extends StatefulWidget {
 
 class _StartGameScreenState extends State<StartGameScreen> {
   int radioValue = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,62 +20,89 @@ class _StartGameScreenState extends State<StartGameScreen> {
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            const Text(
-              'choose your Player X or O',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
-              ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                RadioListTile(
-                  value: radioValue,
-                  groupValue: radioValue,
-                  onChanged: (value) {
-                    setState(() {
-                      radioValue = value!;
-                    });
-                  },
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'choose your Player X or O',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white60,
                 ),
-                RadioListTile(
-                  value: radioValue,
-                  groupValue: radioValue,
-                  onChanged: (value) {
-                    setState(() {
-                      radioValue = value!;
-                    });
-                  },
-                )
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomeScreen(activePlayer: "X"),
-                  ),
-                );
-              },
-              child: const Text(
-                'Start Game',
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: RadioListTile(
+                      title: const Text(
+                        'X',
+                      ),
+                      value: 0,
+                      groupValue: radioValue,
+                      onChanged: (value) {
+                        print(value);
+                        setState(() {
+                          radioValue = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: RadioListTile(
+                      title: const Text(
+                        'O',
+                      ),
+                      value: 1,
+                      groupValue: radioValue,
+                      onChanged: (value) {
+                        print(value);
+                        setState(() {
+                          radioValue = value!;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomeScreen(
+                            activePlayer: radioValue == 0 ? "X" : "O"),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Start Game',
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
